0e22f326cc0a02b4e118d2cf8ca1d1be8376a574
TobiahRex <tobiah.rex@gmail.com>
2017-08-04 21:48:28 +0900

    removed MERGE conflict in render.js

--- render.js
+++ render.js
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
-require('babel-polyfill');
-const React = require( 'react');
-// import React from 'react';
-const { render } = require('react-dom');
-
-const {
-  WebflowComponent,
-} = require('./results/functionalComponent.example/');
-console.log('%cWebflowComponent', 'background:red;', WebflowComponent);
-=======
 import React from 'react';
 import { render } from 'react-dom';
 
 import {
   WebflowComponent,
 } from './results/functionalComponent.example/';
->>>>>>> f8b44095640340ac4b13aa01911fcc7d7d032dc4
 
-render (
+render(
   <WebflowComponent />
-  , document.getElementById('root');
-)
+  , document.getElementById('root'),
+);
